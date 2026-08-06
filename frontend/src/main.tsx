9b5881674e683f98e7880dde8323b600c8f272de
Omar Assaad <omar4assaad@gmail.com>
2026-08-06 19:48:08 +0300

    Add Vercel Web Analytics to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/react"
 import AppRoutes from "./routes/app-routes"
 import { AuthProvider } from "./lib/auth-context"
 import "./index.css"
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
